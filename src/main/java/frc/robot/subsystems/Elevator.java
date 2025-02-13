096083625c49ddabd38a4c212b0e8d3eec0e0f52
Anim Mallon <fieryava@gmail.com>
2025-02-13 17:48:36 -0500

    Mostly convert Elevator to unit system

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,99 +4,192 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import static edu.wpi.first.units.Units.*;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
-import frc.robot.Constants.ElevatorConstants;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.units.DistanceUnit;
+import edu.wpi.first.units.LinearAccelerationUnit;
+import edu.wpi.first.units.LinearVelocityUnit;
+import edu.wpi.first.units.TimeUnit;
+import edu.wpi.first.units.VoltageUnit;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearAcceleration;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Per;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-class ElevatorConfig {
-  // Feedforward
-  public static final double kSVolts = 0.16;
-  public static final double kGVolts = 0.65;
-  public static final double kVVoltSecondPerRad = 0.35;
-  public static final double kAVoltSecondSquaredPerRad = 0.0;
-  public static final double kI = 0;
-  public static final double kP = 0;
-  public static final double kD = 0;
-  public static final double kMaxDegreesPerSecond = 0;
-  public static final double kMaxDegreesPerSecondSquared = 0;
-  public static final double kStoreArmDegrees = 0;
-  public static final double kPositionToleranceDegrees = 0;
-  public static final double kVelocityToleranceDegreesPerSecond = 0;
-  public static final double kIZone = 0;
-  public static final int kIntegratorRange = 0;
-  public static final boolean kMotorZweiInverted = false;
-  public static final boolean kMotorEinsInverted = false;
-  public static final boolean kEncoderEinsInverted = false;
+final class Config {
+  public static final class LeaderMotor {
+    public static final int kCanId = 0;
+    public static final boolean kInverted = false;
+    public static final IdleMode kIdleMode = IdleMode.kBrake;
+    public static final MotorType kMotorType = MotorType.kBrushless;
+  }
+
+  public static final class FollowerMotor {
+    public static final int kCanId = 0;
+    public static final boolean kInverted = false;
+    public static final IdleMode kIdleMode = IdleMode.kBrake;
+    public static final MotorType kMotorType = MotorType.kBrushless;
+  }
+
+  public static final class Encoder {
+    public static final boolean kInverted = false;
+    private static final Distance distancePerSprocketRotation = Inches.of(5.5);
+    private static final double gearboxRatio = 0.1;
+    public static final Distance kPositionConversion = distancePerSprocketRotation.times(gearboxRatio);
+    public static final LinearVelocity kVelocityConversion = kPositionConversion.div(Seconds.of(60));
+  }
+
+  public static final class Feedforward {
+    /**
+     * The coefficient to account for static friction.
+     * <p>
+     * It is multiplied by the sign of the setpoint to get a voltage output.
+     */
+    public static final Voltage kS = Volts.of(0.16);
+    /**
+     * The coefficient to account for gravity.
+     * <p>
+     * It is a constant voltage output regardless of the setpoint's value.
+     */
+    public static final Voltage kG = Volts.of(0.65);
+    /**
+     * The coefficient to handle voltage output based on the setpoint.
+     * <p>
+     * This is multiplied by the setpoint to get a voltage output.
+     * <p>
+     * Unit: <code>volts * seconds / distance</code>
+     * TODO: Convert to unit type
+     */
+    public static final double kV = 0.35;
+  }
+
+  public static final class PID {
+    // Coefficients
+    // TODO: Convert to unit type
+    // Unit is volts / inch
+    public static final Per<VoltageUnit, DistanceUnit> kP = Volts.per(Inch).ofNative(0);
+    // TODO: Convert to unit type
+    // Unit is volts / (inch * second)
+    public static final double kI = 0;
+    // TODO: Convert to unit type
+    // Unit is (volt * second) / inch
+    public static final double kD = 0;
+
+    // Extra config
+    public static final Distance kPositionTolerance = Inches.of(0);
+    public static final LinearVelocity kVelocityTolerance = InchesPerSecond.of(0);
+    public static final Distance kIZone = Inches.of(0);
+    // TODO: Convert to unit type
+    public static final double kIntegratorRange = 0;
+  }
+
+  public static final class Constraints {
+    public static final LinearVelocity kVelocity = InchesPerSecond.of(0);
+    public static final LinearAcceleration kAcceleration = InchesPerSecond.per(Second).of(0);
+  }
+
+  /**
+   * All positions will be distances from the ground to the [center of the
+   * elevator? end of the outtake?]
+   */
+  public static final class Positions {
+    public static final Distance kStartPosition = Inches.of(0);
+    public static final Distance kMinPosition = Inches.of(0);
+    public static final Distance kMaxPosition = Inches.of(50);
+  }
+
+  public static final class Outputs {
+    public static final Voltage kVoltage = Volts.of(0.6);
+  }
+
+  public static final DistanceUnit kDistanceUnit = Inches;
+  public static final LinearVelocityUnit kLinearVelocityUnit = InchesPerSecond;
+  public static final LinearAccelerationUnit kLinearAccelerationUnit = InchesPerSecond.per(Second);
+  public static final VoltageUnit kVoltageUnit = Volts;
+  public static final TimeUnit kTimeUnit = Seconds;
 }
 
 public class Elevator extends SubsystemBase {
+  // #region Member initialization
+  private final SparkMax leaderMotor = new SparkMax(Config.LeaderMotor.kCanId,
+      Config.LeaderMotor.kMotorType);
+  private final SparkMax followerMotor = new SparkMax(Config.FollowerMotor.kCanId,
+      Config.FollowerMotor.kMotorType);
+  private final RelativeEncoder encoder = leaderMotor.getEncoder();
+  private final ElevatorFeedforward feedForward = new ElevatorFeedforward(Config.Feedforward.kS.in(Config.kVoltageUnit),
+      Config.Feedforward.kG.in(
+          Config.kVoltageUnit),
+      Config.Feedforward.kV);
+  private final ProfiledPIDController controller = new ProfiledPIDController(
+      Config.PID.kP.in(Config.kVoltageUnit.per(Config.kDistanceUnit)),
+      Config.PID.kI,
+      Config.PID.kD,
+      new TrapezoidProfile.Constraints(Config.Constraints.kVelocity.in(Config.kLinearVelocityUnit),
+          Config.Constraints.kAcceleration.in(Config.kLinearAccelerationUnit)));
+  // #endregion
 
-  private final SparkMax m_motorEins = new SparkMax(ElevatorConstants.kElevatorEins,
-      com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
-  private final SparkMax m_motorZwei = new SparkMax(ElevatorConstants.kElevatorZwei,
-      com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
-  private final RelativeEncoder m_encoder = m_motorEins.getEncoder();
-  private final ElevatorFeedforward m_feedForward = new ElevatorFeedforward(ElevatorConfig.kSVolts,
-      ElevatorConfig.kGVolts,
-      ElevatorConfig.kVVoltSecondPerRad,
-      ElevatorConfig.kAVoltSecondSquaredPerRad);
-  private final ProfiledPIDController m_controller = new ProfiledPIDController(
-      ElevatorConfig.kP,
-      ElevatorConfig.kI,
-      ElevatorConfig.kD,
-      // The motion profile constraints
-      new TrapezoidProfile.Constraints(ElevatorConfig.kMaxDegreesPerSecond,
-          ElevatorConfig.kMaxDegreesPerSecondSquared));
-
-  /** Creates a new Elevator. */
   public Elevator() {
-    SparkMaxConfig MotorEinsConfig = new SparkMaxConfig();
-    SparkMaxConfig MotorZweiConfig = new SparkMaxConfig();
+    SparkMaxConfig leaderMotorConfig = new SparkMaxConfig();
+    SparkMaxConfig followerMotorConfig = new SparkMaxConfig();
 
-    m_controller.setTolerance(ElevatorConfig.kPositionToleranceDegrees,
-        ElevatorConfig.kVelocityToleranceDegreesPerSecond);
-    m_controller.setIZone(ElevatorConfig.kIZone);
-    m_controller.setIntegratorRange(-ElevatorConfig.kIntegratorRange, ElevatorConfig.kIntegratorRange);
+    leaderMotorConfig
+        .inverted(Config.LeaderMotor.kInverted)
+        .idleMode(Config.LeaderMotor.kIdleMode);
 
-    MotorEinsConfig.inverted(ElevatorConfig.kMotorEinsInverted);
-    MotorZweiConfig.inverted(ElevatorConfig.kMotorZweiInverted);
+    leaderMotorConfig.encoder
+        .inverted(Config.Encoder.kInverted)
+        .positionConversionFactor(Config.Encoder.kPositionConversion.in(Config.kDistanceUnit))
+        .velocityConversionFactor(Config.Encoder.kVelocityConversion.in(Config.kLinearVelocityUnit));
 
-    MotorEinsConfig.encoder.inverted(ElevatorConfig.kEncoderEinsInverted);
-    m_encoder.setPosition(0);
+    followerMotorConfig
+        .follow(leaderMotor, Config.FollowerMotor.kInverted)
+        .idleMode(Config.FollowerMotor.kIdleMode);
 
-    m_motorEins.configure(MotorEinsConfig, null, null);
-    m_motorZwei.configure(MotorZweiConfig, null, null);
+    leaderMotor.configure(leaderMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    followerMotor.configure(followerMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    encoder.setPosition(Config.Positions.kStartPosition.in(Config.kDistanceUnit));
+
+    controller.setTolerance(Config.PID.kPositionTolerance.in(
+        Config.kDistanceUnit),
+        Config.PID.kVelocityTolerance.in(Config.kLinearVelocityUnit));
+    controller.setIZone(Config.PID.kIZone.in(Config.kDistanceUnit));
+    controller.setIntegratorRange(-Config.PID.kIntegratorRange, Config.PID.kIntegratorRange);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+
   }
 
-  public void move(double speed) {
+  public void move(Voltage output) {
     // move the motors the right direction with + being up and - being down
-    m_motorEins.set(speed);
-    m_motorZwei.set(speed);
+    leaderMotor.setVoltage(output);
   }
 
   public void moveUp() {
-    move(0.05);
+    move(Config.Outputs.kVoltage);
   }
 
   public void moveDown() {
-    // call move with a - value
-    move(-0.05);
+    move(Config.Outputs.kVoltage.times(-1));
   }
-  
+
   public void stop() {
-   // call move with a - value
-    move(0.0);
+    move(Config.kVoltageUnit.zero());
   }
 }
-
