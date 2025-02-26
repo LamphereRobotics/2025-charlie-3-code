8706c1cd504920d30839e59ca1a15bc4760a08e1
Ian Morgan <ianhawthorn@gmail.com>
2025-02-25 20:08:24 -0500

    added intake stuff

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,15 +4,43 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ElevatorConstants;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
+  private final SparkMax m_spitMotor = new SparkMax(13,
+      com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+  private final DigitalInput IntakeSensor = new DigitalInput(2);
+
   public Intake() {
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("Intake Sensor", IntakeSensor.get());
   }
+
+  public void in() {
+    if (!IntakeSensor.get()) {
+      m_spitMotor.set(0.15);
+    } else {
+      stop();
+    }
+  }
+
+  public void out() {
+    m_spitMotor.set(0.15);
+  }
+
+  public void stop() {
+    // call move with a - value
+    m_spitMotor.set(0);
+  }
+
 }
