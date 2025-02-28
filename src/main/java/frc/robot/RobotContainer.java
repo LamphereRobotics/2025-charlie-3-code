3717d940e4531ad107bd6ad5e7bcf8540f12f990
Anim Mallon <fieryava@gmail.com>
2025-02-28 17:45:28 -0500

    Remove old drive subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OIConstants;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.CoralIntake;
 import frc.robot.subsystems.YagslDrive;
